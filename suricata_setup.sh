0bdca94b4e0e67ec1b0099af47ac9c179080dcaf
Starke427 <28498535+Starke427@users.noreply.github.com>
2021-02-05 22:57:22 -0700

    Update suricata_setup.sh

--- suricata_setup.sh
+++ suricata_setup.sh
@@ -1,29 +1,31 @@
 #!/bin/bash
 
-# Installs Suricata 4.1.4
+# Installs Suricata
 # Usage: ./suricata_setup.sh [listening_interface: defaults to first interface]
 # Author: Jeff Starke
 
 ### Suricata Installation ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-interface=$(ifconfig | grep -m1 flag | cut -d":" -f1) # Grab server interface
+interface=${1:-$(ifconfig | grep -m1 flag | cut -d":" -f1)} # Grab server interface
 
-# Install and test Suricata-4.1.5
+# Install and test Suricata
 yum install epel-release -y
-yum install suricata-4.1.5 PyYAML -y
+yum install suricata PyYAML -y
 sudo suricata-update
 sudo suricata-update update-sources
 suricata -T
 
 # Configure Suricata listener to restart on reboot with crontab and update at 0200 UTC on Tuesdays
 crontab -l > mycron
-echo "@reboot sudo suricata -c /etc/suricata/suricata.yaml -i ${1-$(ifconfig | grep -m1 flag | cut -d":" -f1)} -D" >> mycron
+echo "@reboot sudo suricata -c /etc/suricata/suricata.yaml -i $interface -D" >> mycron
 echo "* 2 * * * sudo suricata-update" >> mycron
 crontab mycron
 rm -f mycron
 
 # Start Suricata
-sudo suricata -c /etc/suricata/suricata.yaml -i ${1-interface} -D
+sudo suricata -c /etc/suricata/suricata.yaml -i $interface -D
 
 cat << EOF
-Next step is to configure a SPAN/mirror port to forward to ${1-interface} so you can start picking up on lateral network traffic.
+
+Next step is to configure a SPAN/mirror port to forward to $interface so you can start picking up on lateral network traffic.
+
 EOF
